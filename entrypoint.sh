2665f1c7d5c0f2de00c8f203d5f8e8db14b325be
ahbilal-ms <55514721+ahbilal-ms@users.noreply.github.com>
2019-11-08 17:02:31 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # "to avoid continuing when errors or undefined variables are present"
 set -eu
 
-echo "Starting FTP Deploy"
+echo "Starting FTP Upload"
 
 WDEFAULT_LOCAL_DIR=${LOCAL_DIR:-"."}
 WDEFAULT_REMOTE_DIR=${REMOTE_DIR:-"."}
@@ -21,8 +21,8 @@ fi;
 
 echo "Using $WDEFAULT_METHOD to connect to port $WDEFAULT_PORT"
 
-echo "Uploading files..."
-lftp $WDEFAULT_METHOD://$FTP_SERVER:$WDEFAULT_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror $WDEFAULT_ARGS -R $WDEFAULT_LOCAL_DIR $WDEFAULT_REMOTE_DIR; quit"
+echo "Downloading files..."
+lftp $WDEFAULT_METHOD://$FTP_SERVER:$WDEFAULT_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror $WDEFAULT_ARGS -R $WDEFAULT_REMOTE_DIR  $WDEFAULT_LOCAL_DIR; quit"
 
-echo "FTP Deploy Complete"
+echo "FTP Upload Complete"
 exit 0
